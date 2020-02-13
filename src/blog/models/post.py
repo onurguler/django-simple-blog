ae2bb296a715a5e01d60b1ea7e8f00a932d413dd
Onur Güler <guleronurosman@gmail.com>
2020-02-14 01:45:17 +0300

    create post model

---
+++ src/blog/models/post.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    subtitle = models.CharField(max_length=256, blank=True)
+    cover_image = models.ImageField(upload_to='covers', blank=True)
+    text = models.TextField()
+    published_at = models.DateTimeField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title

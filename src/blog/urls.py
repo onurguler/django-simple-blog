50f40e0d5914104b6d2120f76a25c6cd1b6b7e35
Onur Güler <guleronurosman@gmail.com>
2020-02-15 22:03:19 +0300

    drafts

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import index, posts, users
+from blog.views import index, posts, users, drafts
 
 
 app_name = 'blog'
@@ -9,5 +9,7 @@ urlpatterns = [
     path('login/', users.user_login, name='login'),
     path('logout/', users.user_logout, name='logout'),
     path('posts/<int:id>', posts.detail, name='post_detail'),
-    path('posts/new', posts.create, name='post_create')
+    path('posts/new', posts.create, name='post_create'),
+    path('drafts/', drafts.index, name='drafts'),
+    path('drafts/<int:id>', drafts.detail, name='draft_detail')
 ]
---
+++ src/blog/views/drafts.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, get_object_or_404, redirect, reverse, Http404
+from django.core.paginator import Paginator
+from blog.models import Post
+from django.contrib.auth.decorators import login_required
+from blog.forms import PostForm
+
+
+@login_required
+def index(request):
+    posts = Post.objects.filter(published_at__isnull=True).order_by('-created_at')
+
+    # Pagination
+    page_number = request.GET.get('page') or 1
+    per_page = request.GET.get('limit') or 10
+    
+    paginator = Paginator(posts, per_page)
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'drafts/index.html', context={ 'posts': page_obj, 
+                                                   'page_range': paginator.page_range})
+
+
+@login_required
+def detail(request, id):
+    query_set = Post.objects.filter(published_at__isnull=True)
+    post = get_object_or_404(query_set, pk=id)
+
+    return render(request, 'drafts/detail.html', context={ 'post': post })
